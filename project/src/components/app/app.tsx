5dd81c541df28823f93d61c7971aff2997683a39
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-02-14 00:36:20 +0100

    add pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,18 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main-page/main-page';
+// import Favorites from '../favorites/favorites';
+// import FavoritesEmpty from '../favorites-empty/favorites-empty';
+// import Login from '../login/login';
+// import MainPageEmpty from '../main-page-empty/main-page-empty';
+// import Property from '../property/property';
+
+type AppScreenProps = {
+  placesToStay: number;
+}
+
+function App({placesToStay}: AppScreenProps): JSX.Element {
+  return (
+    <Main placesToStay={placesToStay}/>
+  );
 }
 
 export default App;
