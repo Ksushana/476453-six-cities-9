58a53eafce655ab5867fc930a9d88e0d1a44226e
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-02-14 00:37:07 +0100

    clean up

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,4 @@
 import Main from '../main-page/main-page';
-// import Favorites from '../favorites/favorites';
-// import FavoritesEmpty from '../favorites-empty/favorites-empty';
-// import Login from '../login/login';
-// import MainPageEmpty from '../main-page-empty/main-page-empty';
-// import Property from '../property/property';
 
 type AppScreenProps = {
   placesToStay: number;
