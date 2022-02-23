c599de5339cee5ac6f4db9947b2ef593d9646b59
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-02-23 17:13:09 +0100

    added mocs

---
+++ project/src/types/offer.ts
@@ -0,0 +1,12 @@
+export type Offer = {
+  id: number;
+  images: string[];
+  premium: boolean;
+  price: number;
+  title: string;
+  type: string;
+  isFav: boolean;
+  rating: number;
+  rooms: number;
+  maxPers: number;
+};
---
+++ project/src/types/review.ts
@@ -0,0 +1,8 @@
+export type Review = {
+  id: number;
+  text: string;
+  date: string;
+  name: string;
+  photo: string;
+  rating: number;
+};
