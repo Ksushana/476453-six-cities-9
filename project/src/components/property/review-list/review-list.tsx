e58cad5b3ba65010df9b692ecf27cbb0851ab9e5
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-03-07 12:13:14 +0100

    fix

--- project/src/components/property/review-list/review-list.tsx
+++ project/src/components/property/review-list/review-list.tsx
@@ -11,7 +11,7 @@ function ReviewList({reviews}: ReviewsProps) :JSX.Element {
     <>
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
-        {reviews.map((review) => <ReviewItem key={review.id} id={review.id} text={review.text} date={review.date} name={review.name} photo={review.photo} rating={review.rating} />)}
+        {reviews.map((review) => <ReviewItem key={review.id} {...review} />)}
       </ul>
     </>
   );
